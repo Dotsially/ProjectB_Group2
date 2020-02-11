5ca97e423b561ec144a3e583e6aeba49c2a6bfa0
Dotsially <mateu9999@users.noreply.github.com>
2020-02-11 13:02:00 +0100

    Test

---
+++ SideClass.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows;
+
+namespace ProjectB_Group2
+{
+    class SideClass
+    {
+        public static void Func(string a)
+        {
+            int x = 2;
+            int y = 3;
+            while(x < y){
+            }
+              
+        }
+    }
+}
