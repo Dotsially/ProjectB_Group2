9ca7da3ae924e0affa22c21fc905eb6f3b76057c
Dotsially <mateu9999@users.noreply.github.com>
2020-02-21 13:12:23 +0100

    .

--- MainFile.cs
+++ MainFile.cs
@@ -5,7 +5,7 @@ namespace ProjectB_Group2
     class MainFile
     {
         static void Main(string[] args)
-        {hjfht
+        {
             Console.WriteLine("Sally Buns fired!");
         }
     }
