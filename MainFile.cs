88a68e6c553a4967a8e6c679701c703b3898375b
mijosa <0964871@hr.nl>
2020-03-03 12:08:46 +0100

    dfosdfddsssdoihsviovds
    test

--- MainFile.cs
+++ MainFile.cs
@@ -5,8 +5,11 @@ namespace ProjectB_Group2
     class MainFile
     {
         static void Main(string[] args)
-        {hjfht
+        {
             Console.WriteLine("Sally Buns fired!");
+            Console.Beep();
+            Console.WriteLine("hallooooooooooooooooooooooo");
+
         }
     }
 }
